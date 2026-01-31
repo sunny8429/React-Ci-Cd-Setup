feef82f5ee4eb2f1ed39a7dfd028c780622c4dec
sunny8429 <sunmannu8429@gmail.com>
2026-02-01 10:38:25 +1100

    updated test

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -4,5 +4,5 @@ import App from '../App';
 test('render App component', () => {
   render(<App />);
   const headingElement = screen.getByRole('heading', { level: 1 });
-  expect(headingElement).toHaveTextContent(/hello world/i);
+  expect(headingElement).toHaveTextContent(/react ci\/cd project/i);
 });
