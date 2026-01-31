c886e807ae83191f5157e07d11082ab62411f0d5
sunny8429 <sunmannu8429@gmail.com>
2026-01-31 16:33:04 +1100

    add linting in github workflow

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import { test, expect } from 'vitest';
 import App from '../App';
 
 test('render App component', () => {
   render(<App />);
-  const headingElement = screen.getByText(/hello world/i);
-  expect(headingElement).toBeInTheDocument();
+  const headingElement = screen.getByRole('heading', { level: 1 });
+  expect(headingElement).toHaveTextContent(/hello world/i);
 });
